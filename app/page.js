ebc6447d97cd944c4bdccbfac70fa2303958e1be
tsykin <tsikinsasha@gmail.com>
2023-09-21 08:26:06 +0200

    Changed homepage h1

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { data } from "./api/data";
 const page = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1>Data from JSON file inside app</h1>
       <ul>
         {data.map((service) => (
           <li key={service.id}>{service.title}</li>
